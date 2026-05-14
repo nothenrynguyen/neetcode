9c48cc42c15766ba4d009c368b1d706cae96eb3e
nothenrynguyen <69029461+nothenrynguyen@users.noreply.github.com>
2026-05-13 19:51:37 -0700

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // can there b duplicates? non-decreasing order --> yes(?)
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        // check if target is within bounds of each row aka btwn first and last element of row 
+        for (int i = 0; i < matrix.size(); i++) { // iterate thru all rows
+            // perform binary search
+            if (target >= matrix[i][0] && target <= matrix[i][cols-1]) {
+                int left = 0, right = cols-1;
+                int m = (right-left)/2;
+                while (left <= right) {
+                    if (target > matrix[i][left]) {
+                        left++;
+                    }
+                    else if (target < matrix[i][right])
+                        right--;
+                    else
+                        return true;
+                }
+                
+            }
+            else
+                continue;
+        }
+
+        return false;
+    }
+};
